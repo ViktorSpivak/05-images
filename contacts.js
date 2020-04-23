f982c96ea06e3e164d30f5b0358aa0d6c79837cb
ViktorSpivak <49995904+ViktorSpivak@users.noreply.github.com>
2020-04-23 19:03:26 +0300

    first

--- contacts.js
+++ contacts.js
@@ -1,45 +1,64 @@
 const path = require("path");
 const fs = require("fs");
+const { promises: fsPromises } = fs;
 const shortid = require("shortid");
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-exports.listContacts = () =>
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) throw err;
-    console.table(JSON.parse(data));
-  });
-exports.getContactById = (contactId) => {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) throw err;
-    const ContactById = JSON.parse(data).find((el) => el.id === contactId);
-    console.table(ContactById);
-  });
+exports.listContacts = async () =>
+  await fsPromises.readFile(contactsPath, "utf8");
+
+exports.getContactById = async (contactId) => {
+  const contacts = await fsPromises.readFile(contactsPath, "utf8");
+  const ContactById = JSON.parse(contacts).find(
+    (el) => el.id === Number(contactId)
+  );
+  return ContactById;
 };
 
-exports.removeContact = (contactId) => {
-  fs.readFile(contactsPath, "utf8", (err, data) => {
-    if (err) throw err;
-    const listWithRemovedContact = JSON.stringify(
-      JSON.parse(data).filter((el) => el.id !== contactId)
-    );
+exports.removeContact = async (contactId) => {
+  const contacts = await fsPromises.readFile(contactsPath, "utf8");
 
+  const isIdIn = JSON.parse(contacts).some(
+    (el) => el.id.toString() === contactId
+  );
+
+  if (isIdIn) {
+    const listWithRemovedContact = JSON.stringify(
+      JSON.parse(contacts).filter((el) => el.id.toString() !== contactId)
+    );
     fs.writeFile(contactsPath, listWithRemovedContact, (err) => {
       if (err) throw err;
     });
-  });
+  }
+  return isIdIn;
 };
 
-exports.addContact = (name, email, phone) => {
+exports.addContact = async (name, email, phone) => {
   const id = shortid.generate();
   const newContact = { id, name, email, phone };
-  fs.readFile(contactsPath, "utf8", (err, data) => {
+  const contacts = await fsPromises.readFile(contactsPath, "utf8");
+  const listWithAddedContact = JSON.stringify(
+    contacts ? [...JSON.parse(contacts), newContact] : [newContact]
+  );
+  fs.writeFile(contactsPath, listWithAddedContact, (err) => {
     if (err) throw err;
+  });
+  return newContact;
+};
+exports.updateContact = async (id, newProperties) => {
+  const contacts = JSON.parse(await fsPromises.readFile(contactsPath, "utf8"));
+  const updatedContact = contacts.find((el) => el.id.toString() === id);
 
-    const listWithAddedContact = JSON.stringify(
-      data ? [...JSON.parse(data), newContact] : [newContact]
+  if (updatedContact) {
+    newProperties.forEach((el) => (updatedContact[el[0]] = el[1]));
+    const newContacts = JSON.stringify(
+      contacts.map((el) => (el.id.toString() === id ? updatedContact : el))
     );
-    fs.writeFile(contactsPath, listWithAddedContact, (err) => {
+
+    fs.writeFile(contactsPath, newContacts, (err) => {
       if (err) throw err;
     });
-  });
+  }
+
+  return updatedContact;
 };
--- index.js
+++ index.js
@@ -1,27 +1,139 @@
-const contactsActions = require("./contacts");
+const express = require("express");
+const cors = require("cors");
+const Joi = require("@hapi/joi");
+
 const argv = require("yargs").argv;
+const contactsActions = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      contactsActions.listContacts();
-      break;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
 
-    case "get":
-      contactsActions.getContactById(id);
-      break;
+// const isValid = (req, res, next) => {
+//   console.log(req);
 
-    case "add":
-      contactsActions.addContact(name, email, phone);
-      break;
-
-    case "remove":
-      contactsActions.removeContact(id);
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+// const rules = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().required(),
+//   phone: Joi.number().required(),
+// });
+// const validationResult = Joi.validate(req.query, rules);
+// if (validationResult.error) {
+//   res.status(400).header({ message: "missing required name field" });
+//   return res.send(validationResult);
+// }
+//   next();
+// };
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/contacts", async (req, res) => {
+  const allContacts = await contactsActions.listContacts();
+  res.send(allContacts);
+});
+app.get("/contacts/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const contactById = await contactsActions.getContactById(contactId);
+  if (contactById) {
+    res.send(contactById);
+  } else {
+    res.status(404);
+    res.header({ message: "Not found" });
+    res.send();
   }
-}
+});
 
-invokeAction(argv);
+app.post("/contacts", (req, res, next) => {
+  const rules = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+  const validationResult = rules.validate(req.body);
+  if (validationResult.error) {
+    res.status(400).header({ message: "missing required name field" });
+    return res.send(validationResult);
+  }
+  next();
+});
+app.post("/contacts", async (req, res) => {
+  const { name, email, phone } = req.body;
+  const newContact = await contactsActions.addContact(name, email, phone);
+  res.status(201).send(newContact);
+});
+
+app.delete("/contacts/:contactId", async (req, res) => {
+  const { contactId } = req.params;
+  const isIdIn = await contactsActions.removeContact(contactId);
+  if (isIdIn) {
+    res.status(200).send({ message: "contact deleted" });
+  } else {
+    res.status(404);
+    res.header({ message: "Not found" });
+    res.send();
+  }
+});
+
+app.patch("/contacts/:contactId", (req, res, next) => {
+  const hasProperty =
+    req.body.hasOwnProperty("name") ||
+    req.body.hasOwnProperty("email") ||
+    req.body.hasOwnProperty("phone");
+
+  if (!hasProperty) {
+    res.status(400).header({ message: "missing fields" }).send();
+    return;
+  }
+  const rules = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+  const validationResult = rules.validate(req.body);
+  if (validationResult.error) {
+    res.status(400).header({ message: "missing required name field" });
+    return res.send(validationResult);
+  }
+  next();
+});
+
+app.patch("/contacts/:contactId", async (req, res) => {
+  const propertiesForUpdate = Object.entries(req.body);
+  const { contactId } = req.params;
+  const newContact = await contactsActions.updateContact(
+    contactId,
+    propertiesForUpdate
+  );
+  if (newContact) {
+    res.status(200).send(newContact);
+  } else res.header({ message: "Not found" }).status(404).send();
+});
+
+app.listen(3001, () => {
+  console.log("Example app listening on port 3001!");
+});
+// const invokeAction = ({ action, id, name, email, phone }) => {
+//   switch (action) {
+//     case "list":
+//       contactsActions.listContacts();
+//       break;
+
+//     case "get":
+//       contactsActions.getContactById(id);
+//       break;
+
+//     case "add":
+//       contactsActions.addContact(name, email, phone);
+//       break;
+
+//     case "remove":
+//       contactsActions.removeContact(id);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// };
+
+// invokeAction(argv);
